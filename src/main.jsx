e72e2cc8d9948e0d6d620e81d6edd0d4ec7dfdd8
Kaneshkha T <kaneshkhaoffl@gmail.com>
2025-04-06 11:57:39 +0530

    fix router in main

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Playercontextprovider from './context/Playercontext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter basename="/Beginner-Spotify-Clone">
       <Playercontextprovider>
-        <App />
+        <Routes>
+          <Route path="/" element={<App />} />
+        </Routes>
       </Playercontextprovider>
     </BrowserRouter>
   </StrictMode>
